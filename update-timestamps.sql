e4613bb00a97c439b6e91773b5ee08e3f18d0819
System Administrator <root@gp0x6q3hdp.lan>
2022-08-15 10:53:23 -0700

    update

--- update-timestamps.sql
+++ update-timestamps.sql
@@ -11,5 +11,4 @@ select *, dateadd(second, (select date_part(epoch_second, current_timestamp()) -
 Note that we are adjusting the current timestamp in UTC to the demo local time zone America/Los_Angels by a 7 hours delay highlighted in red below, you need to modify this value according to your local time zone
 */
 create or replace view amg_test_db.public.flights_vw as
-select *, dateadd(second, (select date_part(epoch_second, dateadd(hour, -7, current_timestamp())) – date_part(epoch_second, max(old_ts_utc)) from amg_test_db.public.flights), old_ts_utc) as ts_utc from amg_test_db.public.flights ;
-
+select *, dateadd(second, (select date_part(epoch_second, dateadd(hour, -7, current_timestamp())) - date_part(epoch_second, max(old_ts_utc)) from amg_test_db.public.flights), old_ts_utc) as ts_utc from amg_test_db.public.flights;
