30a0b9553027a6eb2ad742ca6b723e6a9f747095
System Administrator <root@gp0x6q3hdp.lan>
2022-08-15 10:45:21 -0700

    update

--- create-tbl.sql
+++ create-tbl.sql
@@ -1,16 +1,15 @@
-USE ROLE SYSADMIN;
-CREATE OR REPLACE WAREHOUSE LOAD_WH WITH WAREHOUSE_SIZE='X-SMALL';
-CREATE DATABASE AMG_TEST_DB;
-CREATE or replace TABLE AMG_TEST_DB.PUBLIC.FLIGHTS (
-        RECORDID NUMBER(38,0),
-        OLD_TS_UTC  TIMESTAMP_NTZ(9),
-        ALT     NUMBER(38,0),
-        DEST    VARCHAR(16777216),
-        ORIG    VARCHAR(16777216),
-        ID      VARCHAR(16777216),
-        ICAO    VARCHAR(16777216),
-        LAT     FLOAT,
-        LON     FLOAT,
-        GEOHASH VARCHAR(16777216)
+use role sysadmin;
+create or replace warehouse load_wh with warehouse_size='x-small';
+create database amg_test_db;
+create or replace table amg_test_db.public.flights (
+        recordid number(38,0),
+        old_ts_utc  timestamp_ntz(9),
+        alt     number(38,0),
+        dest    varchar(16777216),
+        orig    varchar(16777216),
+        id      varchar(16777216),
+        icao    varchar(16777216),
+        lat     float,
+        lon     float,
+        geohash varchar(16777216)
 );
-
